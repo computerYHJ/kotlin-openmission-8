1d4fc4b9764e52c6781b0dfe3f96460ffcbcc3d5
computerYHJ <hyukjin5382@gmail.com>
2025-11-05 20:37:03 +0900

    feat: binding 셋팅, 기존 메인화면과 로그인화면xml 불러옴

--- app/src/main/java/com/example/kotlin_openmission_8/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.kotlin_openmission_8
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.kotlin_openmission_8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        loginBtnClick()
+    }
+
+    private fun loginBtnClick(){
+        with(binding){
+            LoginBtn.setOnClickListener {
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            }
         }
     }
 }
