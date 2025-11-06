0aea3d9305e0dc8482197b16423eaeb6acf1fb98
computerYHJ <hyukjin5382@gmail.com>
2025-11-07 05:11:27 +0900

    feat: 회원가입 시 이메일 입력 및 회원가입 기능 추가

--- app/src/main/java/com/example/kotlin_openmission_8/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/RegisterActivity.kt
@@ -7,10 +7,10 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.example.kotlin_openmission_8.databinding.RegisterBinding
-import com.google.android.material.internal.TextWatcherAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
@@ -20,6 +20,11 @@ class RegisterActivity : AppCompatActivity() {
     private var db = FirebaseFirestore.getInstance()
     private var dbUsers = db.collection("users")
 
+    private var id: String = ""
+    private var password: String = ""
+    private var email: String = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = RegisterBinding.inflate(layoutInflater)
@@ -28,10 +33,28 @@ class RegisterActivity : AppCompatActivity() {
         binding.previousButton.setOnClickListener { finish() }
 
         checkRange(binding.registerIdText, binding.registerIdMessageText, "아이디", 6, 15)
-        binding.registerIdDuplicateBtn.setOnClickListener { duplicateIdBtn() }
+        binding.registerIdDuplicateBtn.setOnClickListener {
+            duplicateBtn(
+                binding.registerIdText,
+                binding.registerIdMessageText,
+                "userID",
+                "아이디"
+            )
+        }
 
+        binding.registerEmailDuplicateBtn.setOnClickListener {
+            duplicateBtn(
+                binding.registerEmailText,
+                binding.registerEmailMessage,
+                "userEmail",
+                "이메일"
+            )
+        }
         checkRange(binding.registerPwdText, binding.registerPwdMessage, "비밀번호", 8, 20)
         notEqualPwd()
+        checkEmail(binding.registerEmailText, binding.registerEmailMessage)
+
+        binding.registerBtn.setOnClickListener { register() }
     }
 
     private fun findWrongInput(input: String, textView: TextView): Boolean {
@@ -43,31 +66,36 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private fun duplicateIdBtn() {
-        var id: String = binding.registerIdText.text.toString()
-        idDuplicate(id)
+    private fun duplicateBtn(editView: EditText, textView: TextView, fields: String, loc: String) {
+        var input: String = editView.text.toString()
+        if (fields == "userID") id = duplicate(input, fields, textView, loc)
+        else if (fields == "userEmail") email = duplicate(input, fields, textView, loc)
     }
 
-    private fun idDuplicate(id: String) {
-        dbUsers.whereEqualTo("userID", id).get().addOnCompleteListener { result ->
-            if (result.isSuccessful) idMessageResult(result.result)
+    private fun duplicate(input: String, fields: String, textView: TextView, loc: String): String {
+        var correct: String = ""
+        dbUsers.whereEqualTo(fields, input).get().addOnCompleteListener { result ->
+            if (result.isSuccessful) {
+                correct = MessageResult(result.result, input, textView, loc)
+            }
         }
+        return correct
     }
 
-    private fun idMessageResult(item: QuerySnapshot) = with(binding) {
-        when {
-            !item.isEmpty -> resisterMessagePrint(
-                registerIdMessageText,
-                "사용할 수 없는 아이디입니다.",
-                Color.RED
-            )
-
-            else -> resisterMessagePrint(registerIdMessageText, "사용할 수 있는 아이디입니다.", Color.BLUE)
+    private fun MessageResult(item: QuerySnapshot, input: String, textView: TextView, loc: String): String {
+        var result: String = ""
+        if (!item.isEmpty) {
+            resisterMessagePrint(textView, "중복된 ${loc}를(을) 입력했습니다.", Color.RED)
+            result = ""
+        } else {
+            if (textView.currentTextColor == Color.BLUE) {
+                resisterMessagePrint(textView, "사용할 수 있는 ${loc}입니다.", Color.BLUE)
+                result = input
+            }
         }
+        return result
     }
-
-
-    private fun resisterMessagePrint(view: TextView, message: String, color: Int) = with(binding) {
+    private fun resisterMessagePrint(view: TextView, message: String, color: Int) {
         view.visibility = View.VISIBLE
         view.text = message
         view.setTextColor(color)
@@ -91,13 +119,27 @@ class RegisterActivity : AppCompatActivity() {
                 target.length > end -> "${loc}는 ${end}자 이하이여야 합니다."
                 else -> ""
             }
-            textView.text = msg
-            textView.visibility = View.VISIBLE
+            if (!msg.isEmpty()) resisterMessagePrint(textView, msg, Color.RED)
+            else resisterMessagePrint(textView, msg, Color.BLUE)
+        }
+    }
+
+    private fun checkEmail(editView: EditText, textView: TextView){
+        editView.addTextChangedListener { s ->
+            var target = s.toString()
+            val emailPattern = android.util.Patterns.EMAIL_ADDRESS
+            var msg = when {
+                !emailPattern.matcher(target).matches() -> "올바른 이메일 형식이 아닙니다"
+                else -> ""
+            }
+            if (!msg.isEmpty()) resisterMessagePrint(textView, msg, Color.RED)
+            else resisterMessagePrint(textView, msg, Color.BLUE)
         }
     }
 
     private fun notEqualPwd() = with(binding) {
         registerSamePwdText.onFocusChangeListener = View.OnFocusChangeListener { view, focus ->
+            registerSamePwdText.filters = arrayOf(InputFilter.LengthFilter(20 + 1))
             val pwd: String = registerPwdText.text.toString()
             val samePwd: String = registerSamePwdText.text.toString()
             if (findWrongInput(samePwd, registerSamePwdMsg)) return@OnFocusChangeListener
@@ -109,17 +151,39 @@ class RegisterActivity : AppCompatActivity() {
     private fun checkSamePwd(pwd: String, samePwd: String) = with(binding) {
         if (pwd.isEmpty()) {
             resisterMessagePrint(registerSamePwdMsg, "비밀번호를 먼저 입력하세요.", Color.RED)
+            password = ""
         } else {
             if (samePwd.isEmpty()) {
                 resisterMessagePrint(registerSamePwdMsg, "비밀번호를 다시 입력하세요.", Color.RED)
+                password = ""
             } else if (pwd != samePwd) {
                 resisterMessagePrint(registerSamePwdMsg, "비밀번호가 일치하지 않습니다.", Color.RED)
+                password = ""
             } else {
                 resisterMessagePrint(registerSamePwdMsg, "", Color.RED)
+                password = samePwd
             }
         }
     }
 
-
-
+    private fun register() {
+        var mAuth = FirebaseAuth.getInstance()
+        Log.d("Register", id + ", " + password)
+        if (!id.isEmpty() && !password.isEmpty() && !email.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+                        dbUsers.add(
+                            User(
+                                userID = id,
+                                userEmail = email,
+                                userPW = password
+                            )
+                        )
+                    }
+                }
+        } else {
+            Toast.makeText(this@RegisterActivity, "잘못된 입력이 존재합니다.", Toast.LENGTH_LONG).show()
+        }
+    }
 }
