0490bd45a7cd123af70d561014d9ac3b40ff7a32
computerYHJ <hyukjin5382@gmail.com>
2025-11-07 16:42:31 +0900

    feat: activity에 있는 검증부분 옮김

--- app/src/main/java/com/example/kotlin_openmission_8/validator/InputValidator.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/validator/InputValidator.kt
@@ -1,5 +1,7 @@
 package com.example.kotlin_openmission_8.validator
 
+import com.google.firebase.firestore.QuerySnapshot
+
 object InputValidator {
 
     fun validatorEmptyField(fields: List<String>): Int{
@@ -8,6 +10,19 @@ object InputValidator {
             fields[0].isEmpty() -> 1
             fields[1].isEmpty() -> 2
             else -> 3
+        }
+    }
+
+    fun validatorInput(input: String, start: Int, end: Int, loc: String): Int{
+        val emailPattern = android.util.Patterns.EMAIL_ADDRESS
+        var regex: Regex = Regex("""[^a-zA-Z0-9]""")
+        return when{
+            loc != "email" && regex.containsMatchIn(input) -> 1
+            loc != "email" && input.length < start -> 2
+            loc != "email" && input.length > end -> 3
+            loc == "email" && !emailPattern.matcher(input).matches() -> 4
+            input.isEmpty() -> 2
+            else -> 99
         }
     }
 }
