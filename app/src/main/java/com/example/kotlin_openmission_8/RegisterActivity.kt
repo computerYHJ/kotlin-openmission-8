bd2fa1bdb7fc13a183c255eb4f1c598d99038275
computerYHJ <hyukjin5382@gmail.com>
2025-11-07 16:51:18 +0900

    feat: UI출력을 분리, 중복 코드 정리

--- app/src/main/java/com/example/kotlin_openmission_8/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/RegisterActivity.kt
@@ -10,10 +10,13 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
 import com.example.kotlin_openmission_8.databinding.RegisterBinding
+import com.example.kotlin_openmission_8.validator.InputValidator
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: RegisterBinding
@@ -24,6 +27,8 @@ class RegisterActivity : AppCompatActivity() {
     private var password: String = ""
     private var email: String = ""
 
+    private var flag: Int = -1
+    private var emailFlag: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,108 +37,86 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.previousButton.setOnClickListener { finish() }
 
-        checkRange(binding.registerIdText, binding.registerIdMessageText, "아이디", 6, 15)
+        checkInput(binding.registerIdText, binding.registerIdMessageText, "아이디", 6, 15)
+        checkInput(binding.registerPwdText, binding.registerPwdMessage, "비밀번호", 8, 20)
+        checkInput(binding.registerEmailText, binding.registerEmailMessage, "email", 8, 20)
+
+        binding.registerEmailDuplicateBtn.setOnClickListener {
+            lifecycleScope.launch {
+                duplicateBtn(
+                    binding.registerEmailText,
+                    binding.registerEmailMessage,
+                    "userEmail",
+                    "이메일"
+                )
+            }
+        }
+
         binding.registerIdDuplicateBtn.setOnClickListener {
-            duplicateBtn(
+            lifecycleScope.launch { duplicateBtn(
                 binding.registerIdText,
                 binding.registerIdMessageText,
                 "userID",
                 "아이디"
-            )
+            ) }
         }
-
-        binding.registerEmailDuplicateBtn.setOnClickListener {
-            duplicateBtn(
-                binding.registerEmailText,
-                binding.registerEmailMessage,
-                "userEmail",
-                "이메일"
-            )
-        }
-        checkRange(binding.registerPwdText, binding.registerPwdMessage, "비밀번호", 8, 20)
         notEqualPwd()
-        checkEmail(binding.registerEmailText, binding.registerEmailMessage)
-
         binding.registerBtn.setOnClickListener { register() }
     }
 
-    private fun findWrongInput(input: String, textView: TextView): Boolean {
-        return if (Regex("""[^a-zA-Z0-9]""").containsMatchIn(input)) {
-            resisterMessagePrint(textView, "형식에 맞는 입력이 아닙니다.", Color.RED)
-            true
-        } else {
-            false
-        }
-    }
-
-    private fun duplicateBtn(editView: EditText, textView: TextView, fields: String, loc: String) {
+    private suspend fun duplicateBtn(editView: EditText, textView: TextView, fields: String, loc: String) {
         var input: String = editView.text.toString()
-        if (fields == "userID") id = duplicate(input, fields, textView, loc)
-        else if (fields == "userEmail") email = duplicate(input, fields, textView, loc)
-    }
-
-    private fun duplicate(input: String, fields: String, textView: TextView, loc: String): String {
-        var correct: String = ""
-        dbUsers.whereEqualTo(fields, input).get().addOnCompleteListener { result ->
-            if (result.isSuccessful) {
-                correct = MessageResult(result.result, input, textView, loc)
+        Log.d("CHECK INPUT ERROR", "$flag, $emailFlag")
+        when(fields){
+            "userID" -> when(duplicate(fields, input, textView, loc)){
+                1 -> if(flag == 99) id = input
+                else -> id = ""
+            }
+            "userEmail" -> when(duplicate(fields, input, textView, loc)){
+                1 -> if(emailFlag == 99) email = input
+                else -> email = ""
             }
         }
-        return correct
     }
 
-    private fun MessageResult(item: QuerySnapshot, input: String, textView: TextView, loc: String): String {
-        var result: String = ""
-        if (!item.isEmpty) {
-            resisterMessagePrint(textView, "중복된 ${loc}를(을) 입력했습니다.", Color.RED)
-            result = ""
-        } else {
-            if (textView.currentTextColor == Color.BLUE) {
-                resisterMessagePrint(textView, "사용할 수 있는 ${loc}입니다.", Color.BLUE)
-                result = input
-            }
+    private suspend fun duplicate(
+        fields: String,
+        input: String,
+        textView: TextView,
+        loc: String
+    ): Int{
+        return when(UserRepository.checkDupliceate(input,fields)) {
+            1 -> 1.also{resisterMessagePrint(textView, "사용할 수 있는 ${loc}입니다.", Color.BLUE)}
+            2 -> 2.also{resisterMessagePrint(textView, "${loc}가(이) 입력되지 않았습니다.", Color.RED)}
+            3 -> 3.also{resisterMessagePrint(textView, "중복된 ${loc}를(을) 입력했습니다.", Color.RED)}
+            else -> 0
         }
-        return result
     }
+
     private fun resisterMessagePrint(view: TextView, message: String, color: Int) {
         view.visibility = View.VISIBLE
         view.text = message
         view.setTextColor(color)
     }
 
-    private fun checkRange(
+    private fun checkInput(
         editView: EditText,
         textView: TextView,
         loc: String,
         start: Int,
         end: Int
-    ) = with(binding) {
+    ) {
         editView.addTextChangedListener { s ->
             editView.filters = arrayOf(InputFilter.LengthFilter(end + 1))
             var target = s.toString()
 
-            if (findWrongInput(target, textView)) return@addTextChangedListener
-
-            var msg = when {
-                target.length < start -> "${loc}는 ${start}자 이상이여야 합니다."
-                target.length > end -> "${loc}는 ${end}자 이하이여야 합니다."
-                else -> ""
+            when (InputValidator.validatorInput(target, start, end, loc)) {
+                1 -> flag = 1.also { resisterMessagePrint(textView, "형식에 맞는 입력이 아닙니다.", Color.RED) }
+                2 -> flag = 2.also { resisterMessagePrint(textView, "${loc}는 ${start}자 이상 입력해주세요.", Color.RED) }
+                3 -> flag = 3.also { resisterMessagePrint(textView, "${loc}는 ${start}자 이하로 입력해주세요.", Color.RED) }
+                4 -> emailFlag = 4.also { resisterMessagePrint(textView, "이메일 형식에 맞지 않습니다.", Color.RED) }
+                else -> flag = 99.also { textView.visibility = View.INVISIBLE; emailFlag = 99 }
             }
-            if (!msg.isEmpty()) resisterMessagePrint(textView, msg, Color.RED)
-            else resisterMessagePrint(textView, msg, Color.BLUE)
-        }
-    }
-
-    private fun checkEmail(editView: EditText, textView: TextView){
-        editView.addTextChangedListener { s ->
-            var target = s.toString()
-            val emailPattern = android.util.Patterns.EMAIL_ADDRESS
-            var msg = when {
-                !emailPattern.matcher(target).matches() -> "올바른 이메일 형식이 아닙니다"
-                else -> ""
-            }
-            if (!msg.isEmpty()) resisterMessagePrint(textView, msg, Color.RED)
-            else resisterMessagePrint(textView, msg, Color.BLUE)
         }
     }
 
@@ -142,7 +125,6 @@ class RegisterActivity : AppCompatActivity() {
             registerSamePwdText.filters = arrayOf(InputFilter.LengthFilter(20 + 1))
             val pwd: String = registerPwdText.text.toString()
             val samePwd: String = registerSamePwdText.text.toString()
-            if (findWrongInput(samePwd, registerSamePwdMsg)) return@OnFocusChangeListener
             if (focus) checkSamePwd(pwd, samePwd)
             else checkSamePwd(pwd, samePwd)
         }
