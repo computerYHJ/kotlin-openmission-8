6758ab0f2b08c4b936127fd93037281dd326db45
computerYHJ <hyukjin5382@gmail.com>
2025-11-09 01:24:58 +0900

    feat: 로그인 관련 firebase 기능 추가

--- app/src/main/java/com/example/kotlin_openmission_8/UserRepository.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/UserRepository.kt
@@ -1,6 +1,10 @@
 package com.example.kotlin_openmission_8
 
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.coroutines.tasks.await
 
 object UserRepository {
@@ -21,4 +25,33 @@ object UserRepository {
             }
         } catch (e: Exception){ 0 }
     }
+
+    suspend fun register(id: String, password: String, email: String){
+        val mAuth = FirebaseAuth.getInstance()
+        val dbUsers = db.collection("users")
+        try{
+            mAuth.createUserWithEmailAndPassword(email, password).await()
+            dbUsers.add(User(userID = id, userPW = password, userEmail = email)).await()
+        } catch (e: Exception) {
+            Log.e("REGISTER ERROR", "회원가입이 실패하였습니다.")
+        }
+    }
+
+    suspend fun attemptLogin(id: String, pwd: String): Int {
+        return try {
+            val doc = db.collection("users").whereEqualTo("userID", id).get().await()
+
+            if (doc.isEmpty) { 1 }
+            else {
+                val info = doc.documents.first()
+                val email = info.getString("userEmail") ?: return 2
+
+                val result = FirebaseAuth.getInstance().signInWithEmailAndPassword(email, pwd).await()
+
+                if (result.user != null) 99
+                else 0
+            }
+        } catch (e: Exception) { 0 }
+    }
+
 }
