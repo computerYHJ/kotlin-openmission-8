fe578ebb1bb206efa231d41e6f264867150535a2
computerYHJ <hyukjin5382@gmail.com>
2025-11-15 03:43:26 +0900

    fix: 이용권의 종료날짜가 없을 시 break되는 버그 수정

--- app/src/main/java/com/example/kotlin_openmission_8/User.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/User.kt
@@ -1,15 +1,24 @@
 package com.example.kotlin_openmission_8
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 data class User(
     var userID: String = "",
     var userPW: String = "",
     var userName: String = "",
     var userEmail: String = "",
-    var startWorkout: String = "2025-11-12",
-    var endWorkout: String = "",
+    var startWorkout: String = today(),
+    var endWorkout: String = today(),
     var startTime: String = "",
     var endTime: String = "",
     var monthGoal: Int = 0,
     var workoutCount: Int = 0
-)
+){
+    companion object {
+        fun today(): String =
+            LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+    }
+}
+
 
